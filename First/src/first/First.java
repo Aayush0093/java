82d323c614780bbd6b8a32fc5b56668bb687a297
Aayush0093 <115064348+Aayush0093@users.noreply.github.com>
2024-02-25 10:51:06 +0530

    Update First.java

--- First/src/first/First.java
+++ First/src/first/First.java
@@ -15,7 +15,7 @@ public class First {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello Wolrd");
+        System.out.println("Hello World");
        
     }
     
