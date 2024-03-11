54ef3db10fd1fde696e203a4bbe22450e1f05ac7
Aayush0093 <aayushbobbytyagi@gmail.com>
2024-03-12 05:25:50 +0530

    Documentation

---
+++ javadocdemo/dist/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"book","l":"book"},{"p":"javadocdemo","l":"Javadocdemo"}];updateSearchResults();
---
+++ javadocdemo/src/book/book.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package book;
+
+/**
+ * @author aayushtyagi
+ * @version 1.O
+ * @since 2023
+ */
+
+/**
+ * class for library book
+ * @author aayushtyagi
+ */
+public class book {
+    
+    /**
+     * static integer assigned value as 1
+     */
+    static int a =10;
+    
+    /**
+     * Constructor 
+     * accepts a string 
+     * @param s 
+     */
+    public book(String s){}
+    
+    
+    /**
+     * Issue book to student
+     * @param roll for info of student
+     * @throws Exception 
+     */
+    public void issue(int roll)throws Exception{}
+    
+    /**
+     * get details
+     * @param id student id
+     * @return name of the student
+     */
+    public String getName(int id){return "";}
+    
+    /**
+     * Checks whether book there or not
+     * @return true if available else false
+     */
+    public boolean available(){ return true;}
+    
+    
+}
