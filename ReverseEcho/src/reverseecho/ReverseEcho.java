22294ff3f2e4fd70a3b1248142f95f26cb729c66
Aayush0093 <aayushbobbytyagi@gmail.com>
2024-03-27 05:32:12 +0530

    Reverse Echo

---
+++ ReverseEcho/src/reverseecho/ReverseEcho.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package reverseecho;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.*;
+
+/**
+ *
+ * @author aayushtyagi
+ */
+public class ReverseEcho {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+        // TODO code application logic here
+        
+        ServerSocket ss = new ServerSocket(2000);
+        Socket stk = ss.accept();
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(stk.getInputStream()));
+        PrintStream ps = new PrintStream(stk.getOutputStream());
+        
+        String msg;
+        StringBuilder sb;
+        
+        do{
+            msg = br.readLine();
+            sb = new StringBuilder(msg);
+            sb.reverse();
+            msg = sb.toString();
+            ps.println(msg);
+            
+        }while(!msg.equals("dne"));
+    }
+    
+}
+
+class Client {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+        // TODO code application logic here
+        
+        Socket stk = new Socket("localhost",2000);
+        
+        BufferedReader keyb = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(stk.getInputStream()));
+        PrintStream ps = new PrintStream(stk.getOutputStream());
+        
+        String msg;
+        
+        do{
+            msg = keyb.readLine();
+            ps.println(msg);
+            msg = br.readLine();
+            
+            System.out.println("From SERVER : "+msg);
+            
+        }while(!msg.equals("dne"));
+        
+        stk.close();
+    }
+    
+}
